5d9ca0b0cd67c58642a91ce2940b2cbde902d870
lucascjesus6-pixel <lucas.cjesus6@aluno.edu.es.gov.br>
2025-08-21 02:33:24 -0300

    Add files via upload

---
+++ data/emergencyContacts.js
@@ -0,0 +1,5 @@
+export const emergencyContacts = [
+  { name: "Delegacia da Mulher - ES", phone: "180", location: [-19.8157, -43.9542] },
+  { name: "ONG Acolher", phone: "0800 123 456", location: [-19.8257, -43.9642] },
+  { name: "Hospital Mulher Segura", phone: "0800 654 321", location: [-19.8357, -43.9742] },
+];
